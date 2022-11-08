44fcafc2fc3f624290b107bb22028e2f9df2724b
Florencia Praderi <flopraderi@gmail.com>
2022-11-08 17:33:53 +0100

    Curso Angular Udemy

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = "Florencia";
+let edad: number | string = 29;
+let mujer: boolean = true;
+
+edad = "thirty"
+
+console.log(nombre, edad, mujer)
+
--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,8 +1,18 @@
-
-
-console.log('Hola Mundo!');
-
 /*
     ===== Código de TypeScript =====
 */
 
+function sumar(a: number, b: number): number{
+    return a + b;
+}
+
+const sumarFlecha = (a: number, b: number): number =>{
+    return a + b;
+}
+
+function multiplicar(num1: number, num2?: number, base:number = 2): number{
+    return num1 * base;
+}
+
+const resultado = multiplicar(4, 4)
+console.log(resultado)
